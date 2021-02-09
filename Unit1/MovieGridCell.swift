0fe0a1668ebd7efac0387807b8bd6f6c320969a3
Ryan Saini <ryansaini@Ryans-MBP.attlocal.net>
2021-02-08 22:13:21 -0800

    test

---
+++ Unit1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Unit1
+//
+//  Created by Ryan Saini on 2/8/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
